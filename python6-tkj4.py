34d764d2f1eae7210c6569f75c7b8e03b86685ce
latiefbachrululum <112526246+latiefbachrululum@users.noreply.github.com>
2022-09-05 09:56:42 +0700

    Add files via upload

---
+++ python6-tkj4.py
@@ -0,0 +1,36 @@
+# list kosong
+list_kosong = []
+
+#list yang berisi kumpulan string
+list_buah = ['Pisang', 'Nanas', 'Melon', 'Durian']
+
+# list yang berisi kumpulan integer
+list_nilai = [80, 70, 90, 60]
+
+# list campuran berbagai tipe data
+list_jawaban = [150, 33.33, 'Presiden Sukarno', False]
+
+print('list_kosong:', list_kosong)
+print('list_buah:', list_buah)
+print('list_nilai:', list_nilai)
+print('list_jawaban:', list_jawaban)
+
+print(list_buah[0])
+print(list_buah[2])
+print(list_buah[1])
+print(list_buah[3])
+
+Pisang
+Melon
+Nanas
+Durian
+
+print(list_buah[-1])
+print(list_buah[-2])
+print(list_buah[-3])
+print(list_buah[-4])
+
+Durian
+Melon
+Nanas
+Pisang
