9d75d48c58ef0197bf3c500a87a0d47c54643dea
latiefbachrululum <112526246+latiefbachrululum@users.noreply.github.com>
2022-09-05 08:36:05 +0700

    Add files via upload

---
+++ python5-tkj4.py
@@ -0,0 +1,23 @@
+# dollar ke rupiah
+dollar = 3
+rupiah = 44.691
+jumlah_dr = 3 * 44.691
+print("jumlah mata uang adalah",jumlah_dr)
+
+# uero ke yen
+euro = 10
+yen = 139.2
+jumlah_ey = 10 * 139.2
+print("jumlah mata uang adalah",jumlah_ey)
+
+# won ke rupiah
+won = 100
+rupiah = 10.92
+jumlah_wr = 100 * 10.92
+print("jumlah mata uang adalah",jumlah_wr)
+
+
+
+
+
+
